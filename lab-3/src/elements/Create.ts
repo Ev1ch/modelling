ad6c834b1cbbab066b7c0ed32933e01fb82d9e1d
Vlad Zusko <vladzvu11@gmail.com>
2023-10-22 01:18:21 +0300

    feat: add distribution for create

--- lab-3/src/elements/Create.ts
+++ lab-3/src/elements/Create.ts
@@ -1,15 +1,22 @@
+import Distribution from '../Distribution';
 import Element from './Element';
 import Variation from './Variation';
 
 interface CreateOptions {
   variation: Variation;
+  distribution: Distribution;
 }
 
 export default class Create extends Element {
-  constructor(name: string, delay: number, { variation }: CreateOptions) {
+  constructor(
+    name: string,
+    delay: number,
+    { variation, distribution }: CreateOptions,
+  ) {
     super(name, delay);
     this.tNext = 0;
     this.variation = variation;
+    this.distribution = distribution;
   }
 
   public outAct() {
