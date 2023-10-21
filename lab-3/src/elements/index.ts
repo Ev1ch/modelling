05dde22b2db0724f4fb5487af18e645263b3298d
Vlad Zusko <vladzvu11@gmail.com>
2023-10-22 01:59:51 +0300

    feat: add queue re-export

--- lab-3/src/elements/index.ts
+++ lab-3/src/elements/index.ts
@@ -1,3 +1,4 @@
 export { default as Element } from './Element';
 export { default as Create } from './Create';
 export { default as Process } from './Process';
+export { default as Queue } from './Queue';
