d85df219fdb4d4762ad18b5d1529ee6ec76dbbd4
Vlad Zusko <vladzvu11@gmail.com>
2023-10-22 01:53:15 +0300

    feat: add different random types

--- lab-3/src/Random.ts
+++ lab-3/src/Random.ts
@@ -10,4 +10,38 @@ export default class Random {
 
     return number;
   }
+
+  public static uniform(min: number, max: number) {
+    let number = 0;
+
+    while (number === 0) {
+      number = Math.random();
+    }
+
+    number = min + number * (max - min);
+
+    return number;
+  }
+
+  public static normal(mean: number, deviation: number) {
+    let number = 0;
+
+    for (let i = 0; i < 12; i++) {
+      number += Math.random();
+    }
+
+    number -= 6;
+
+    return mean + number * deviation;
+  }
+
+  public static erlang(mean: number, deviation: number) {
+    let number = 0;
+
+    for (let i = 0; i < deviation; i++) {
+      number += Random.exponential(mean);
+    }
+
+    return -1 / deviation / mean + Math.log(number);
+  }
 }
