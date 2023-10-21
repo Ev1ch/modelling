eb9c56331348a9262cbbeb38355cbda2e92756f2
Vlad Zusko <vladzvu11@gmail.com>
2023-10-22 00:58:40 +0300

    feat: add create variation

--- lab-3/src/elements/Create.ts
+++ lab-3/src/elements/Create.ts
@@ -1,9 +1,19 @@
 import Element from './Element';
+import Variation from './Variation';
+
+interface CreateOptions {
+  variation?: Variation;
+}
 
 export default class Create extends Element {
-  constructor(name: string, delay: number) {
+  constructor(
+    name: string,
+    delay: number,
+    { variation = Variation.PROBABILISTIC }: CreateOptions,
+  ) {
     super(name, delay);
     this.tNext = 0;
+    this.variation = variation;
   }
 
   public outAct() {
