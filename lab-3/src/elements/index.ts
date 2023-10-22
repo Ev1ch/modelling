dd6497814538de7e80363a6586b4b24e3c00c477
Vlad Zusko <vladzvu11@gmail.com>
2023-10-22 17:13:55 +0300

    feat: add variation export

--- lab-3/src/elements/index.ts
+++ lab-3/src/elements/index.ts
@@ -3,3 +3,4 @@ export { default as Create } from './Create';
 export { default as Process } from './Process';
 export { default as Queue } from './Queue';
 export { default as Delay } from './Delay';
+export { default as Variation } from './Variation';
