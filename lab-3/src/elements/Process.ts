2ddedf6276955cf2910e907fcac4acd9508e7778
Vlad Zusko <vladzvu11@gmail.com>
2023-10-24 09:15:07 +0300

    feat: add delay wit h processor in process

--- lab-3/src/elements/Process.ts
+++ lab-3/src/elements/Process.ts
@@ -1,8 +1,18 @@
-import Element from './Element';
+import Element, { type NextElement } from './Element';
 import Worker, { WorkerState } from './Worker';
 import Variation from './Variation';
 import Queue from './Queue';
 import Delay from './Delay';
+
+interface DelayWithProcessor<TItem> {
+  delay: Delay;
+  condition: (item: TItem) => boolean;
+  processor: (item: TItem) => void;
+}
+
+interface NextElementWithCondition<TItem> extends NextElement<TItem> {
+  condition?: (item: TItem) => boolean;
+}
 
 export interface ProcessOptions {
   maxWorkersNumber: number;
@@ -10,21 +20,23 @@ export interface ProcessOptions {
   minimumDifferenceToSwap: number;
 }
 
-export default class Process extends Element {
-  private _queue: Queue;
+export default class Process<TItem> extends Element<TItem> {
+  private _queue: Queue<TItem>;
   private _failuresNumber: number;
   private _maxWorkersNumber: number;
   private _swapsNumber: number;
   private _minimumDifferenceToSwap: number;
-  private _workers: Worker[];
+  private _workers: Worker<TItem>[];
   private _queueTime: number;
   private _workingTime: number;
-  private _neighbors: Process[];
+  private _neighbors: Process<TItem>[];
+  private _totalTimeBeforeIn: number;
+  private _tPrevious: number;
 
   constructor(
     name: string,
-    delays: { delay: Delay; probability: number }[],
-    queue: Queue,
+    delays: DelayWithProcessor<TItem>[],
+    queue: Queue<TItem>,
     { maxWorkersNumber, variation, minimumDifferenceToSwap }: ProcessOptions,
   ) {
     super(name, delays);
@@ -39,18 +51,30 @@ export default class Process extends Element {
     this._neighbors = [];
     this._swapsNumber = 0;
     this._minimumDifferenceToSwap = minimumDifferenceToSwap;
+    this._totalTimeBeforeIn = 0;
+    this._tPrevious = 0;
 
     for (let i = 0; i < this._maxWorkersNumber; i++) {
       this._workers.push(new Worker(i));
     }
   }
 
-  public inAct() {
+  public get totalTimeBeforeIn() {
+    return this._totalTimeBeforeIn;
+  }
+
+  public inAct(item: TItem) {
     const freeWorker = this.getFreeWorker();
 
+    this._totalTimeBeforeIn += this.tCurrent - this._tPrevious;
+    this._tPrevious = this.tCurrent;
+
     if (freeWorker) {
+      const delayWithProcessor = this.getDelayWithCondition(item);
+      const delay = delayWithProcessor.delay.get();
       freeWorker.state = WorkerState.BUSY;
-      const delay = this.getDelay().get();
+      delayWithProcessor.processor(item);
+      freeWorker.item = item;
       freeWorker.tNext = this.tCurrent + delay;
       this._workingTime += delay;
       this.tNext = freeWorker.tNext;
@@ -58,7 +82,7 @@ export default class Process extends Element {
     }
 
     if (!this._queue.isFull()) {
-      this._queue.addItem();
+      this._queue.addItem(item);
       return;
     }
 
@@ -74,13 +98,15 @@ export default class Process extends Element {
       throw new Error('There is no busy workers');
     }
 
+    const item = busyWorker.item;
     busyWorker.tNext = Infinity;
-    this.tNext = this.getMinimumWorkersTNext();
     busyWorker.state = WorkerState.FREE;
+    busyWorker.item = null;
+    this.tNext = this.getMinimumWorkersTNext();
 
     this.swapQueueItems();
 
-    const fullNextElement = this.getFullNextElement();
+    const fullNextElement = this.getFullNextElement(item);
 
     if (
       fullNextElement &&
@@ -88,20 +114,24 @@ export default class Process extends Element {
       fullNextElement.withBlocking
     ) {
       busyWorker.tNext = fullNextElement.element.tNext;
+      busyWorker.item = item;
       busyWorker.state = WorkerState.BUSY;
       return;
     }
 
     if (!this._queue.isEmpty()) {
-      this._queue.removeItem();
+      const item = this._queue.removeItem();
+      const delayWithProcessor = this.getDelayWithCondition(item);
+      const delay = delayWithProcessor.delay.get();
       busyWorker.state = WorkerState.BUSY;
-      const delay = this.getDelay().get();
+      delayWithProcessor.processor(item);
+      busyWorker.item = item;
       busyWorker.tNext = this.tCurrent + delay;
       this._workingTime += delay;
       this.tNext = this.getMinimumWorkersTNext();
     }
 
-    fullNextElement?.element.inAct();
+    fullNextElement?.element.inAct(item);
   }
 
   private swapQueueItems() {
@@ -110,19 +140,42 @@ export default class Process extends Element {
         this._queue.size - neighbor.queue.size >=
         this._minimumDifferenceToSwap
       ) {
-        neighbor.queue.addItem();
-        this._queue.removeItem();
+        const item = this._queue.removeLastItem();
+        neighbor.queue.addItem(item);
         this._swapsNumber++;
         return;
       }
     }
   }
 
+  protected getNextElement(item: TItem) {
+    switch (this.variation) {
+      case Variation.PROBABILISTIC:
+        return this.getNextElementByProbability();
+      case Variation.PRIORITIZED:
+        return this.getNextElementByPriority();
+      case Variation.RANDOM:
+        return this.getRandomNextElement();
+      case Variation.BY_CONDITION:
+        return this.getNextElementByCondition(item);
+      default:
+        throw new Error('Wrong variation type');
+    }
+  }
+
+  public get delays() {
+    return super.delays as DelayWithProcessor<TItem>[];
+  }
+
+  public set delays(delays: DelayWithProcessor<TItem>[]) {
+    super.delays = delays;
+  }
+
   public get queue() {
     return this._queue;
   }
 
-  public set queue(queue: Queue) {
+  public set queue(queue: Queue<TItem>) {
     this._queue = queue;
   }
 
@@ -150,7 +203,7 @@ export default class Process extends Element {
     return this._neighbors;
   }
 
-  public set neighbors(neighbors: Process[]) {
+  public set neighbors(neighbors: Process<TItem>[]) {
     this._neighbors = neighbors;
   }
 
@@ -160,6 +213,14 @@ export default class Process extends Element {
 
   public get workers() {
     return this._workers;
+  }
+
+  public get nextElements() {
+    return super.nextElements as NextElementWithCondition<TItem>[];
+  }
+
+  public set nextElements(nextElements: NextElementWithCondition<TItem>[]) {
+    super.nextElements = nextElements;
   }
 
   public isFree() {
@@ -202,5 +263,37 @@ export default class Process extends Element {
 
   private getMinimumWorkersTNext() {
     return Math.min(...this._workers.map((worker) => worker.tNext));
+  }
+
+  private getDelayWithCondition(item: TItem) {
+    for (const delay of this.delays) {
+      if (delay.condition(item)) {
+        return delay;
+      }
+    }
+
+    throw new Error('Wrong delay');
+  }
+
+  public static getDelayWithCondition<TItem>(
+    delay: Delay,
+    condition: (item: TItem) => boolean,
+    processor = (item: TItem) => {},
+  ) {
+    return { delay, condition, processor };
+  }
+
+  private getNextElementByCondition(item: TItem) {
+    if (this.nextElements.length === 0) {
+      return null;
+    }
+
+    for (const { element, condition } of this.nextElements) {
+      if (condition(item)) {
+        return element;
+      }
+    }
+
+    return null;
   }
 }
